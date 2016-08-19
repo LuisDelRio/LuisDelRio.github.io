86eaf970c01bbaf0b963513719cfec76d1bdf179
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-18 20:04:37 -0500

    Update Ejercicio_2.js

--- rv/Ejercicio_2.js
+++ rv/Ejercicio_2.js
@@ -1,10 +1,10 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 10;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
-var formaesf = new THREE.SphereGeometry(1, 20, 20);
+var formaesf = new THREE.SphereGeometry(5, 20, 20);
 var formacil = new THREE.CylinderGeometry(1, 1, 2, 20, 2, false);
 var material = new THREE.MeshNormalMaterial();
 var esfera = new THREE.Mesh(formaesf, material);
