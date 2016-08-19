2d0a0751a1b78c5aec322f1278975752499bbfab
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-18 20:08:17 -0500

    Update Ejercicio_2.js

--- rv/Ejercicio_2.js
+++ rv/Ejercicio_2.js
@@ -5,7 +5,7 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 var formaesf = new THREE.SphereGeometry(3, 20, 20);
-var formacil = new THREE.CylinderGeometry(1, 1, 2, 20, 2, false);
+var formacil = new THREE.CylinderGeometry(1, 1, 10, 20, 2, false);
 var material = new THREE.MeshNormalMaterial();
 var esfera = new THREE.Mesh(formaesf, material);
 var cilindro = new THREE.Mesh( formacil, material);
