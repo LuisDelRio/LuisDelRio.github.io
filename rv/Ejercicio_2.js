4f22953b19312d9b6d24ee57d98b44af1e3d0e64
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-18 19:32:52 -0500

    Update Ejercicio_2.js

--- rv/Ejercicio_2.js
+++ rv/Ejercicio_2.js
@@ -5,9 +5,7 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
-var cyl_width = 1;
-var cyl_height = 5;
-var forma = new THREE.CylinderGeometry(cyl_width, cyl_width, cyl_height, 20, 1, false);
+var forma = new THREE.CylinderGeometry(1, 1, 5, 10, 1, false);
 
 var material = new THREE.MeshNormalMaterial();
 var cilindro = new THREE.Mesh( forma, material);
