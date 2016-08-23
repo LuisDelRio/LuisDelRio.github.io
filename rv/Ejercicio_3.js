cd0ba3b3a619393500b4b9009c6d5ccc33000479
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-23 18:27:37 -0500

    Create Ejercicio_3.js

---
+++ rv/Ejercicio_3.js
@@ -0,0 +1,31 @@
+var forma = new THREE.Geometry();
+
+forma.vertices.push( new THREE.Vector3( 1, -1, 1) );
+forma.vertices.push( new THREE.Vector3( 1, -1, -1) );
+forma.vertices.push( new THREE.Vector3( -1, -1, -1) );
+forma.vertices.push( new THREE.Vector3( -1, -1, 1) );
+forma.vertices.push( new THREE.Vector3( 1, 1, 1) );
+forma.vertices.push( new THREE.Vector3( 1, 1, -1) );
+forma.vertices.push( new THREE.Vector3( -1, 1, -1) );
+forma.vertices.push( new THREE.Vector3( -1, 1, 1) );
+
+forma.faces.push( new THREE.Face3( 1, 2, 3 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 1 ) );
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render ( escena, camara );
