37ed068b798f446ed26678fca546b9b524eeed3b
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-23 19:47:50 -0500

    Update Ejercicio_3.js

--- rv/Ejercicio_3.js
+++ rv/Ejercicio_3.js
@@ -26,6 +26,7 @@ forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
 escena.add( malla );
 var camara = new THREE.PerspectiveCamera();
