44b0e7722dfc51cbcb2c106ed8679c52e0b3ed6d
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-25 15:29:41 -0500

    Create Ejemplo_extrusion.js

---
+++ rv/Ejemplo_extrusion.js
@@ -0,0 +1,20 @@
+var figura = new THREE.shape();
+figura.moveTo(10,20);
+figura.lineTo(10,40);
+figura.lineTo(40,40);
+figura.lineTo(10,10);
+
+var forma = new THREE.ExtrudeGeometry( figura, {amount: 10} );
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/4 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+var camar = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.ineerHeight*.95, window.innerHeight*.95);
+document.body.appendChilda(renderizador.domElement);
+renderizador.render(escena, camara);
