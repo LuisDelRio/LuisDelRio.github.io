a19fb8f68e09e6bfad5dab66b23ec4193e06ec3e
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-30 15:23:58 -0500

    Update Torre_ajedrez.js

--- rv/Torre_ajedrez.js
+++ rv/Torre_ajedrez.js
@@ -13,7 +13,7 @@ var torreMalla = new THREE.Mesh(torreForma, material);
 var escena = new THREE.Scene();
 escena.add(torreMalla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=100;
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
