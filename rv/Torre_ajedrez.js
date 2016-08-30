ec81c9f71184014e5e94c68a846080a68c2f52cb
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-30 15:29:04 -0500

    Update Torre_ajedrez.js

--- rv/Torre_ajedrez.js
+++ rv/Torre_ajedrez.js
@@ -2,7 +2,7 @@ var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
 var base2Forma = new THREE.CylinderGeometry(4,4,1.5,20,1.5,false);
 var base3Forma = new THREE.CylinderGeometry(4,3,2,20,2,false);
 base2Forma.translate(0,1,0);
-base2Forma.translate(0,2.5,0);
+base3Forma.translate(0,2.5,0);
 var base1Malla = new THREE.Mesh(base1Forma);
 var base2Malla= new THREE.Mesh(base2Forma);
 var base3Malla= new THREE.Mesh(base3Forma);
