8e92d3db744b73c875a0817094e2baa1e1fadb07
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-30 20:05:57 -0500

    Update Ejercicio_4.js

--- rv/Ejercicio_4.js
+++ rv/Ejercicio_4.js
@@ -6,7 +6,7 @@ figura.lineTo(10, 0);
 figura.lineTo(30, 0);
 figura.lineTo(5, -15);
 figura.lineTo(15, -35);
-figura.lineTo(0, 0);
+figura.lineTo(0, -28.96);
 figura.lineTo(-15, -35);
 figura.lineTo(-5, -15);
 figura.lineTo(-30, 0);
