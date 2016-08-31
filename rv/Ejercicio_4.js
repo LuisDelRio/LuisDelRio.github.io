c359f73ccfc407756eb968fca9e0e1102d6fe0ca
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-30 19:38:52 -0500

    Update Ejercicio_4.js

--- rv/Ejercicio_4.js
+++ rv/Ejercicio_4.js
@@ -1,9 +1,9 @@
 var figura = new THREE.Shape();
 
-figura.moveTo(0, 0);
-figura.lineTo(10, 40);
-figura.lineTo(40, 40);
-figura.lineTo(10, 10);
+figura.moveTo(-10, 0);
+figura.lineTo(0, 10);
+figura.lineTo(10, 0);
+figura.lineTo(-10, 0);
 
 var forma = new THREE.ShapeGeometry(figura);
 var malla = new THREE.Mesh(forma);
