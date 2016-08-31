4af9d367c0a709d79b37b1545880bcccbb15bc9a
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-08-30 20:02:24 -0500

    Update Ejercicio_4.js

--- rv/Ejercicio_4.js
+++ rv/Ejercicio_4.js
@@ -1,18 +1,16 @@
 var figura = new THREE.Shape();
 
-figura.moveTo(0, 2);
-figura.lineTo(1, 2);
-figura.lineTo(2, 0);
-figura.lineTo(4, 0);
-figura.lineTo(3, -2);
-figura.lineTo(4, -3);
-figura.lineTo(2, -3);
-figura.lineTo(1, -5);
-figura.lineTo(0, -3);
-figura.lineTo(-2, -3);
-figura.lineTo(-1, -2);
-figura.lineTo(-2, 0);
+figura.moveTo(-10, 0);
+figura.lineTo(0, 20);
+figura.lineTo(10, 0);
+figura.lineTo(30, 0);
+figura.lineTo(5, -15);
+figura.lineTo(15, -35);
 figura.lineTo(0, 0);
+figura.lineTo(-15, -35);
+figura.lineTo(-5, -15);
+figura.lineTo(-30, 0);
+figura.lineTo(-10, 0);
 
 var forma = new THREE.ShapeGeometry(figura);
 var malla = new THREE.Mesh(forma);
@@ -20,7 +18,7 @@ var malla = new THREE.Mesh(forma);
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 10;
+camara.position.z = 100;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
