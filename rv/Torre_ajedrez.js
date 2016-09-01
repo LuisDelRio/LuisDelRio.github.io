c1de19392bb7ff5d58fc186afc8a95496bb68f12
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-01 15:12:55 -0500

    Update Torre_ajedrez.js

--- rv/Torre_ajedrez.js
+++ rv/Torre_ajedrez.js
@@ -6,7 +6,7 @@ var cubierta1Forma = new THREE.CylinderGeometry(4,3,2,20,2,false);
 var cubierta2Forma = new THREE.CylinderGeometry(4,4,2,20,3,false);
 
 var arco = new THREE.Shape();
-arco.moveTo(-20,0);
+arco.moveTo(-10,0);
 arco.arc(3.75,1.36, 4, 20,-20);
 arco.lineTo(3.75,1.36);
 
