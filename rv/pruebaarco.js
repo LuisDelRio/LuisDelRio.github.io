133ca194b65a7daf33c7c3aff551a4cd844c5d73
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-01 15:28:13 -0500

    Update pruebaarco.js

--- rv/pruebaarco.js
+++ rv/pruebaarco.js
@@ -1,17 +1,5 @@
-var curve = new THREE.EllipseCurve(
-    0, 0,             // ax, aY
-    7, 15,            // xRadius, yRadius
-    0, 3/2 * Math.PI, // aStartAngle, aEndAngle
-    false             // aClockwise
-);
-
-var points = curve.getSpacedPoints( 20 );
-
-var path = new THREE.Path();
-var geometry = path.createGeometry( points );
-
-var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-
-var line = new THREE.Line( geometry, material );
-
-scene.add( line );
+var arco = new THREE.Shape();
+arco.arc(3.75,1.36, 4, 20,-20);
+arco.lineTo(3.75,-1.36);
+var pico1 = new THREE.ExtrudeGeometry( arco, {amount: 5} );
+pico1.rotateY( Math.PI/4 );
