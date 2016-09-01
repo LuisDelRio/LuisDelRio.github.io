80081c1efc3bbbd6cc004c07aa9a1e7271ca30e7
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-01 15:34:57 -0500

    Update pruebaarco.js

--- rv/pruebaarco.js
+++ rv/pruebaarco.js
@@ -1,16 +1,17 @@
-var arco = new THREE.Shape();
-arco.arc(3.75,1.36, 4, 20,-20);
-arco.lineTo(3.75,-1.36);
+var figura = new THREE.Shape();
 
-var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( arco, material );
+figura.moveTo(0, 0);
+figura.arc(3.75, 1.36, 4, 20, 340);
+figura.lineTo(3.75, -1.36);
 
+var forma = new THREE.ShapeGeometry(figura);
+var malla = new THREE.Mesh(forma);
 
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 100;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
-document.body.appendChild(renderizador.domElement);
-renderizador.render(escena, camara);
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
