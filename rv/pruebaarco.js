30341be559cf296882487c3d2e2735c18cc8dd76
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-01 15:29:39 -0500

    Update pruebaarco.js

--- rv/pruebaarco.js
+++ rv/pruebaarco.js
@@ -10,7 +10,7 @@ malla.rotateX( Math.PI/4 );
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 100;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
