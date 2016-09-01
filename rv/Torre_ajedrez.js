c982970dc839e5d0edf9004420ddb3311a3cc22b
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-01 15:27:03 -0500

    Update Torre_ajedrez.js

--- rv/Torre_ajedrez.js
+++ rv/Torre_ajedrez.js
@@ -9,7 +9,7 @@ var arco = new THREE.Shape();
 arco.arc(3.75,1.36, 4, 20,-20);
 arco.lineTo(3.75,-1.36);
 var pico1 = new THREE.ExtrudeGeometry( arco, {amount: 5} );
-pico1.rotateX( Math.PI/4 );
+pico1.rotateY( Math.PI/4 );
 
 
 base2Forma.translate(0,1,0);
