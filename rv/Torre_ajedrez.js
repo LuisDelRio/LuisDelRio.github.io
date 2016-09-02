823cc7935a859c858e4e288d80f20f7d6c0fc812
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-01 19:25:05 -0500

    Update Torre_ajedrez.js

--- rv/Torre_ajedrez.js
+++ rv/Torre_ajedrez.js
@@ -72,8 +72,8 @@ torreForma.merge(pico4Malla.geometry, pico4Malla.matrix);
 var material= new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh(torreForma, material);
 
-torreMalla.rotateX(-Math.PI/4);
-torreMalla.rotateY(Math.PI/4);
+torreMalla.rotateX(Math.PI/4);
+torreMalla.rotateY(-Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add(torreMalla);
