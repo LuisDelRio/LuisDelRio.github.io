48c9b5a3812a5570ba82310ef9fbf8d6900ff7da
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-08 15:12:37 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,12 +1,19 @@
-var campoVision = 45;
+var campoVision = 100;
 var relacionAspecto = window.innerWidth / window.innerWidth;
-var planoCercano = 1;
-var planoLejano = 1000;
+var planoCercano = 100;
+var planoLejano = 100;
 var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
-camara.position.z=15;
+camara.position.z=50;
 
-var cubo = new THREE.Mesh( new THREE.BoxGeometry(2, 2, 2), new THREE.MeshNormalMaterial() );
-cubo.rotateX(Math.PI/4);
+for (i = 0; i < 8; i++) {
+  for (j = 0; j < 8; j++){
+    var cubo = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 10), new THREE.MeshNormalMaterial() );
+    cubo[i,j].rotateX(Math.PI/4);
+    cubo[i,j].position.x=i+10;
+  }
+  cubo[i,j].position.y=j+10;
+}
+
 var esfera1 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
 esfera1.position.x=5;
 var esfera2= new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
