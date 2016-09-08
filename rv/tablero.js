32c2dd6de9f28cb3f0f1f638f08c3ef83ada6253
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-08 15:29:31 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -5,24 +5,30 @@ var planoLejano = 100;
 var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
 camara.position.z=50;
 
-var cubo[8,8] = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 10), new THREE.MeshNormalMaterial() );
-for (i = 0; i < 8; i++) {
-  for (j = 0; j < 8; j++){
-    cubo[i,j].rotateX(Math.PI/4);
-    cubo[i,j].position.x=i+10;
+var cubo = new Array("c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", 
+                     "c11", "c12", "c13", "c14", "c15", "c16", "c17", "c18", "c19", "c20",
+                     "c21", "c22", "c23", "c24", "c25", "c26", "c27", "c28", "c29", "c30",
+                     "c31", "c32", "c33", "c34", "c35", "c36", "c37", "c38", "c39", "c40",
+                     "c41", "c42", "c43", "c44", "c45", "c46", "c47", "c48", "c49", "c50", 
+                     "c51", "c52", "c53", "c54", "c55", "c56", "c57", "c58", "c59", "c60",
+                     "c61", "c62", "c63", "c64");
+for (k = 0; k < 64; k++) {    
+  if (k%2=0) {
+    var cubo[k] = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 10), new THREE.MeshBasicMaterial(0xffffff) );
+  }else{
+    var cubo[k] = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 10), new THREE.MeshBasicMaterial(0x888888) );
   }
-  cubo[i,j].position.y=j+10;
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++){
+      cubo[k].rotateX(Math.PI/4);
+      cubo[k].position.x=i*10;
+      cubo[k].position.y=j*10;
+    }
+  }
 }
 
-var esfera1 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
-esfera1.position.x=5;
-var esfera2= new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
-esfera2.position.x=-5;
-esfera2.position.z=-10;
 
 var escena = new THREE.Scene();
-escena.add(esfera1);
-escena.add(esfera2);
 escena.add(cubo);
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth, window.innerHeight);
