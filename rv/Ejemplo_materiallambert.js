a45b59bbfbf226caeaf0f90fd574482cdc0a529d
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-13 14:58:41 -0500

    Update Ejemplo_materiallambert.js

--- rv/Ejemplo_materiallambert.js
+++ rv/Ejemplo_materiallambert.js
@@ -1,5 +1,5 @@
 var forma = new THREE.SphereGeometry(1);
-var material = new THREE.MeshLambertMaterial({color: '#00cc00"});
+var material = new THREE.MeshLambertMaterial({color: '#00cc00'});
 var malla = new THREE.Mesh(forma, material);
 
 var escena= new THREE.Scene();
