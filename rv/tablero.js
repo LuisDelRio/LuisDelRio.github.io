f753e658351f7ced6fe5f3a031308341451ec9e8
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-13 23:51:30 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -73,7 +73,7 @@ torreForma.merge(pico4Malla.geometry, pico4Malla.matrix);
 var material= new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh(torreForma, material);
 
-torreMalla.translateOnAxis(z,2);
+torreMalla.translateOnAxis('z',2);
 
 
 //Tablero
