e9dbd8fca8275a58b39038a4c77388c7ab0af21b
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-13 20:00:43 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -3,10 +3,7 @@ var relacionAspecto = window.innerWidth / window.innerWidth;
 var planoCercano = 100;
 var planoLejano = 1000;
 var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
-camara.position.z=150;
-camara.position.y=-100;
-camara.position.x=150;
-
+camara.position.z=15;
 
 var escena = new THREE.Scene();
 var cubo = new Array();
