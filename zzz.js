ed7ef92522472dedfe5b874e83ed96eef2cec7be
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-13 20:53:30 -0500

    Update zzz.js

--- zzz.js
+++ zzz.js
@@ -1,1 +1,23 @@
-wew
+var campoVision = 45;
+var relacionAspecto = window.innerWidth / window.innerWidth;
+var planoCercano = 1;
+var planoLejano = 1000;
+var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
+camara.position.z=15;
+
+var cubo = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 2), new THREE.MeshNormalMaterial() );
+cubo.rotateY(Math.PI/4);
+var cubo1 = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 2), new THREE.MeshNormalMaterial() );
+esfera1.position.x=5;
+var cubo2= new THREE.Mesh(new THREE.BoxGeometry(10, 10, 2), new THREE.MeshNormalMaterial());
+esfera2.position.x=-5;
+esfera2.position.z=-10;
+
+var escena = new THREE.Scene();
+escena.add(cubo1);
+escena.add(cubo2);
+escena.add(cubo);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
