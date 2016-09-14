8bb6b23def54902fac15c28cf0ee09ed2b3d86f2
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-13 22:13:09 -0500

    Update zzz.js

--- zzz.js
+++ zzz.js
@@ -4,8 +4,8 @@ var planoCercano = 1;
 var planoLejano = 1000;
 var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
 camara.position.z=250;
-camara.position.x=320;
-camara.position.y=320;
+camara.position.x=40;
+camara.position.y=40;
 
 var escena = new THREE.Scene();
 var cubo= new Array();
