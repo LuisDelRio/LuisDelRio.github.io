933e118cb46b71c7147e53b02d8a734e40486c1a
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-13 19:46:43 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -8,14 +8,14 @@ camara.position.z=15;
 var escena = new THREE.Scene();
 var cubo = new Array();
 
-for (k = 0; k < 64; k++) {    
+for (var k = 0; k < 64; k++) {    
   if (k%2==0) {
     cubo[k] = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 2), new THREE.MeshBasicMaterial({color: 0xFFFFFF}) );
   }else{
     cubo[k] = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 2), new THREE.MeshBasicMaterial({color: 0x888888}) );
   }
-  for (i = 0; i < 8; i++) {
-    for (j = 0; j < 8; j++){
+  for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++){
       cubo[k].rotateX(Math.PI/4);
       cubo[k].position.x=i*10;
       cubo[k].position.y=j*10;
