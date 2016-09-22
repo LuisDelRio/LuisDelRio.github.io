616734c3dc99a0e04f9b0c55af42e77ef64213a3
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-22 15:13:07 -0500

    Update ejemplo_funcion.js

--- rv/ejemplo_funcion.js
+++ rv/ejemplo_funcion.js
@@ -1,4 +1,10 @@
 function init (p){
+  var campoVision = 45;
+  var relacionAspecto = window.innerWidth / window.innerWidth;
+  var planoCercano = 1;
+  var planoLejano = 1000;
+  var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
+  camara.position.z=5;
   var malla = new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
   escena = new THREE.Scene();
   escena.add(malla);
