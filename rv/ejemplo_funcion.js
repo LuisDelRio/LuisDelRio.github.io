2169fe809c6a3846ccdac416ab3ee62d1b713601
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-22 15:17:35 -0500

    Update ejemplo_funcion.js

--- rv/ejemplo_funcion.js
+++ rv/ejemplo_funcion.js
@@ -1,10 +1,9 @@
 function init (p){
-  var camara = new THREE.PerspectiveCamera();
-  camara.position.z=5*p;
   var malla = new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
   escena = new THREE.Scene();
   escena.add(malla);
   camara = new THREE.PerspectiveCamera();
+  camara.position.z=5*p;
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
   document.body.appendChild(renderizador.domElement);
