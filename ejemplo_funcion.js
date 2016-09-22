50436a0b393b858fd6b5633b9b13bb7261ed93a5
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-22 15:06:48 -0500

    Update and rename rv/ejemplo_funcion.txt to ejemplo_funcion.js

--- ejemplo_funcion.js
+++ ejemplo_funcion.js
@@ -5,8 +5,13 @@ function init (p){
   camara = new THREE.PerspectiveCamera();
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
+  document.body.appendChild(renderizador.domElement);
   }
   
   var main = function(p){
     renderizador.render(escena, camara);
   }
+
+var escena, camara, renderizador;
+init(1);
+main(2);
