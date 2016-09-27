3eb9b74f8e145c2dce2e9c472bdb5bd0b05863f8
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-27 15:00:30 -0500

    Update ejemplo_ventana.js

--- rv/ejemplo_ventana.js
+++ rv/ejemplo_ventana.js
@@ -7,7 +7,7 @@ VENTANA.listener=function(){
 VENTANA.setup=function(){
   var tipo_evento='resize';
   var capturarp=false;
-  window.addEventListener(tipo_evento, VENATANA.listener, capturarp); 
+  window.addEventListener(tipo_evento, VENTANA.listener, capturarp); 
   VENTANA.escena=new THREE.Scene();
   VENTANA.camara=new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
   VENTANA.camara.position.z=5;
