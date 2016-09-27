23b31a086f9f67e4a6bda0e2b017593b146aa7b5
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-09-27 15:39:41 -0500

    Update ejemplo_textura.js

--- rv/ejemplo_textura.js
+++ rv/ejemplo_textura.js
@@ -8,12 +8,12 @@ TEXTURA.retrollamada=function(textura){
 TEXTURA.setup=function(){
   TEXTURA.escena=new THREE.Scene();
   var cargador = new THREE.TextureLoader();
-  cargador.load("earth_atmos_2048.jpg", TEXTURE.retrollamada);
+  cargador.load("earth_atmos_2048.jpg", TEXTURA.retrollamada);
   TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   TEXTURA.camara.position.z=5;
   var lienzo=document.getElemtById("ejemplo_textura");
   TEXTURA.renderizador=new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
-  TEXTURE.renderizador.setSize(600,600);
+  TEXTURA.renderizador.setSize(600,600);
 }
 TEXTURA.loop=function(){
   requestAnimationFrame(TEXTURA.loop);
