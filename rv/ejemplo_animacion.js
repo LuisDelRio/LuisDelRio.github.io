1ce07bdc7e4d6af7a67f0b7b7ca3d34ab825285b
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-10-20 15:27:30 -0500

    Update ejemplo_animacion.js

--- rv/ejemplo_animacion.js
+++ rv/ejemplo_animacion.js
@@ -19,7 +19,7 @@ function setup(){
   escena= new THREE.Scene();
   escena.add(pieza);
   camara = new THREE.PerspectiveCamera();
-  camara.position.z=5;
+  camara.position.z=20;
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
   document.body.appendChild(renderizador.domElement); 
