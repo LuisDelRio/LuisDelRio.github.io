a4c86cddff168e6a2d832aa30a54fb23125e94fa
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-11-30 22:25:29 -0600

    Update Proyecto_Ajedrez.js

--- rv/Proyecto_Ajedrez.js
+++ rv/Proyecto_Ajedrez.js
@@ -680,13 +680,13 @@ function loop(){
   if(cuyo==3){
     if(seleccionadorMalla.position.x==0 && seleccionadorMalla.position.y==0){
       valor[seleccionadorMalla.position.x][seleccionadorMalla.position.y]=valor[0][0];
-      valor[0][0].position.x=seleccionadorMalla.position.x;
-      valor[0][0].position.y=seleccionadorMalla.position.y;
+      valor[0][0].translateX(seleccionadorMalla.position.x);
+      valor[0][0].translateZ(seleccionadorMalla.position.y);
       valor[0][0]="";                 
     }else if(seleccionadorMalla.position.x==0 && seleccionadorMalla.position.y==1){
       valor[seleccionadorMalla.position.x][seleccionadorMalla.position.y]=valor[0][1];
-      valor[0][1].position.x=seleccionadorMalla.position.x;
-      valor[0][1].position.y=seleccionadorMalla.position.y;
+      valor[0][1].translateX(seleccionadorMalla.position.x);
+      valor[0][1].translateZ(seleccionadorMalla.position.y);
       valor[0][1]="";
     }
     seleccionadorMalla.position.x=0;
