ca59876f43fea7db0fb3314f27ef2b225da002a9
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-04 15:36:47 -0600

    Update Ajedrez_prueba.js

--- rv/Ajedrez_prueba.js
+++ rv/Ajedrez_prueba.js
@@ -41,37 +41,77 @@ Environment.prototype.act = function(){
 
 function Torre(textura){ 
     Agent.call(this);
-    var puntos=[];
-    
-    puntos.push(new THREE.Vector2(0,0));
-    puntos.push(new THREE.Vector2(20,0));
-    puntos.push(new THREE.Vector2(20,10));
-    puntos.push(new THREE.Vector2(15,10));
-    puntos.push(new THREE.Vector2(15,15));
-    puntos.push(new THREE.Vector2(10,15));
-    puntos.push(new THREE.Vector2(10,40));
-    puntos.push(new THREE.Vector2(20,40));
-    puntos.push(new THREE.Vector2(20,45));
-    puntos.push(new THREE.Vector2(10,45));
-    puntos.push(new THREE.Vector2(10,50));
-    puntos.push(new THREE.Vector2(15,50));
-    puntos.push(new THREE.Vector2(15,60));
-    puntos.push(new THREE.Vector2(0,60));
-    
-    var baseForma= new THREE.LatheGeometry(puntos);
-    var baseMalla = new THREE.Mesh(baseForma);
+  var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
+  var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
+  var base3Forma = new THREE.CylinderGeometry(3,4,2,20,2,false);
+  var troncoForma = new THREE.CylinderGeometry(3,3,6,20,6,false);
+  var cubierta1Forma = new THREE.CylinderGeometry(4,3,2,20,2,false);
+  var cubierta2Forma = new THREE.CylinderGeometry(4,4,2,20,3,false);
 
-    var torreForma= new THREE.Geometry();
+  var arco1 = new THREE.Shape();
+  arco1.moveTo(0, 0);
+  arco1.arc(0, 0, 4, .52, -.52, true);
+  arco1.lineTo(3.46, -2);
+  var pico1 = new THREE.ExtrudeGeometry( arco1, {amount: 1, bevelEnabled: false});
+  pico1.rotateX(Math.PI/2);
 
-    torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+  var arco2 = new THREE.Shape();
+  arco2.moveTo(0, 0);
+  arco2.arc(0, 0, 4, .52, -.52, true);
+  arco2.lineTo(3.46, -2);
+  var pico2 = new THREE.ExtrudeGeometry( arco2, {amount: 1, bevelEnabled: false});
+  pico2.rotateX(Math.PI/2);
+  pico2.rotateY(Math.PI/2);
 
-    for(var i=0; i<=6; i++){
-    var picoForma = new THREE.CylinderGeometry( 4, 2, 15, 32 );
-      picoForma.translate(13*(Math.sin(Math.PI*2/6*i)),60,13*(Math.cos(Math.PI*2/6*i)));
-    var picoMalla =new THREE.Mesh(picoForma); 
-      torreForma.merge(picoMalla.geometry, picoMalla.matrix);
-    }
-    
+  var arco3 = new THREE.Shape();
+  arco3.moveTo(0, 0);
+  arco3.arc(0, 0, 4, .52, -.52, true);
+  arco3.lineTo(3.46, -2);
+  var pico3 = new THREE.ExtrudeGeometry( arco3, {amount: 1, bevelEnabled: false});
+  pico3.rotateX(Math.PI/2);
+  pico3.rotateY(Math.PI);
+
+  var arco4 = new THREE.Shape();
+  arco4.moveTo(0, 0);
+  arco4.arc(0, 0, 4, .52, -.52, true);
+  arco4.lineTo(3.46, -2);
+  var pico4 = new THREE.ExtrudeGeometry( arco4, {amount: 1, bevelEnabled: false});
+  pico4.rotateX(Math.PI/2);
+  pico4.rotateY(Math.PI*3/2);
+
+  base2Forma.translate(0,1,0);
+  base3Forma.translate(0,2,0);
+  troncoForma.translate(0,4,0);
+  cubierta1Forma.translate(0,8,0);
+  cubierta2Forma.translate(0,10,0);
+  pico1.translate(0,12,0);
+  pico2.translate(0,12,0);
+  pico3.translate(0,12,0);
+  pico4.translate(0,12,0);
+  
+
+  var base1Malla = new THREE.Mesh(base1Forma);
+  var base2Malla= new THREE.Mesh(base2Forma);
+  var base3Malla= new THREE.Mesh(base3Forma);
+  var toncoMalla= new THREE.Mesh(troncoForma);
+  var cubierta1Malla= new THREE.Mesh(cubierta1Forma);
+  var cubierta2Malla= new THREE.Mesh(cubierta2Forma);
+  var pico1Malla= new THREE.Mesh(pico1);
+  var pico2Malla= new THREE.Mesh(pico2);
+  var pico3Malla= new THREE.Mesh(pico3);
+  var pico4Malla= new THREE.Mesh(pico4);
+  
+  var torreForma = new THREE.Geometry();
+  torreForma.merge(base1Malla.geometry, base1Malla.matrix);
+  torreForma.merge(base2Malla.geometry, base2Malla.matrix);
+  torreForma.merge(base3Malla.geometry, base3Malla.matrix);
+  torreForma.merge(toncoMalla.geometry, toncoMalla.matrix);
+  torreForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
+  torreForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
+  torreForma.merge(pico1Malla.geometry, pico1Malla.matrix);
+  torreForma.merge(pico2Malla.geometry, pico2Malla.matrix);
+  torreForma.merge(pico3Malla.geometry, pico3Malla.matrix);
+  torreForma.merge(pico4Malla.geometry, pico4Malla.matrix);
     //THREE.Mesh.call(this, torreForma, new THREE.MeshLambertMaterial({map:textura}));
     this.add(new THREE.Mesh(torreForma, new THREE.MeshLambertMaterial({map:textura})));
     this.castShadow=true;
@@ -173,31 +213,25 @@ function TorrePlan(x,y,team){
 //------------PEON----------
 var Peon=function(textura){    
     Agent.call(this);
-    var puntospeon=[];
-    
-    puntospeon.push( new THREE.Vector2(0,0));
-    puntospeon.push( new THREE.Vector2(20,0));
-    puntospeon.push( new THREE.Vector2(20,10));
-    puntospeon.push( new THREE.Vector2(15,10));
-    puntospeon.push( new THREE.Vector2(15,15));
-    puntospeon.push( new THREE.Vector2(10,15));
-    puntospeon.push( new THREE.Vector2(5,25));
-    puntospeon.push( new THREE.Vector2(5,35));
-    puntospeon.push( new THREE.Vector2(15,35));
-    puntospeon.push( new THREE.Vector2(0,40));
+    var base1Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
+  var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
+  var troncoForma = new THREE.CylinderGeometry(1.5,3,7,7,7,false);
+  var cabezaForma = new THREE.SphereGeometry(2);
 
-    var basePeonForma= new THREE.LatheGeometry(puntospeon);
-    var basePeonMalla = new THREE.Mesh(basePeonForma);
+  base2Forma.translate(0,1,0);
+  troncoForma.translate(0,5,0);
+  cabezaForma.translate(0,9.5,0);
 
-    var peonForma= new THREE.Geometry();
+  var base1Malla = new THREE.Mesh(base1Forma);
+  var base2Malla= new THREE.Mesh(base2Forma);
+  var troncoMalla= new THREE.Mesh(troncoForma);
+  var cabezaMalla= new THREE.Mesh(cabezaForma);
 
-    peonForma.merge(basePeonMalla.geometry, basePeonMalla.matrix);
-
-    var puntaPeonForma = new THREE.SphereGeometry(10);
-    puntaPeonForma.translate(0,45,0);
-    var puntaPeonMalla =new THREE.Mesh(puntaPeonForma); 
-    peonForma.merge(puntaPeonMalla.geometry, puntaPeonMalla.matrix);
-        
+  var peonForma = new THREE.Geometry();
+  peonForma.merge(base1Malla.geometry, base1Malla.matrix);
+  peonForma.merge(base2Malla.geometry, base2Malla.matrix);
+  peonForma.merge(troncoMalla.geometry, troncoMalla.matrix);
+  peonForma.merge(cabezaMalla.geometry, cabezaMalla.matrix);
     //THREE.Mesh.call(this, peonForma, new THREE.MeshLambertMaterial({map:textura}));
     this.add(new THREE.Mesh(peonForma, new THREE.MeshLambertMaterial({map:textura})));
     this.castShadow=true;
