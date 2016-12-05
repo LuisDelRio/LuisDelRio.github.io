57b491dec5596a46178b24da9d48eb35d3cc16b6
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-05 17:58:07 -0600

    Update Ajedrez_prueba_jorge.js

--- rv/Ajedrez_prueba_jorge.js
+++ rv/Ajedrez_prueba_jorge.js
@@ -151,7 +151,7 @@ function Torreplan(x0, y0, xf, yf, side){
   var side = parseInt(side);
   if(xf==x0 && y0<=yf){
 	  //alert("primerif");
-     for(var i=0; i==yf; i++){
+     for(var i=0; i<=yf; i++){
 	   alert("primerfor");  
         if(yf!=y0){
 	  alert("segundoif");	
