cfe4f75db95e36d82f2dfea86410cc4115adedc9
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-06 17:31:49 -0600

    Update Ajedrez_prueba_jorge.js

--- rv/Ajedrez_prueba_jorge.js
+++ rv/Ajedrez_prueba_jorge.js
@@ -159,6 +159,7 @@ function Torreplan(x0, y0, xf, yf, side){
      for(i=0; i<=yf; i++){  
         if(yf!=y0){	
 	  piezaActual.position.y+=1;
+		requestAnimationFrame(Torreplan);
   	}else if(yf==y0){
 		valor[xfs][yfs]= piezaActual;
 	        valor[x0s][y0s]= piezaPosterior;
@@ -205,7 +206,7 @@ function Torreplan(x0, y0, xf, yf, side){
 	     flag=flag+1;
 	  }
 	
-  requestAnimationFrame(Torreplan);
+  requestAnimationFrame(loop);
   renderizador.render(escena,camara);
 }
 
