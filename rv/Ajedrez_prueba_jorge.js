379b785a6b919fcdc8a36bd87bea39e94ccc652f
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-07 21:01:53 -0600

    Update Ajedrez_prueba_jorge.js

--- rv/Ajedrez_prueba_jorge.js
+++ rv/Ajedrez_prueba_jorge.js
@@ -715,27 +715,27 @@ function setup(){
   caballoMalla2 = new Caballo(TEXTURAS.ceramicanegra);
   caballoMalla3 = new Caballo(TEXTURAS.ceramicablanca);
 
-  caballoMalla.rotateY(-Math.PI/2);
   caballoMalla.rotateX(Math.PI/2);
   caballoMalla.translateY(3);
   caballoMalla.translateX(10);
+  caballoMalla.rotate.z=-Math.PI/2;
 
-  caballoMalla1.rotateY(Math.PI/2);
   caballoMalla1.rotateX(Math.PI/2);
   caballoMalla1.translateY(3);
   caballoMalla1.translateZ(-70);
   caballoMalla1.translateX(10);
+  caballoMalla.rotate.z=Math.PI/2;
 
-  caballoMalla2.rotateY(Math.PI/2);
   caballoMalla2.rotateX(Math.PI/2);
   caballoMalla2.translateY(3);
   caballoMalla2.translateZ(-70);
   caballoMalla2.translateX(60);
-  
-  caballoMalla3.rotateY(-Math.PI/2);
+  caballoMalla.rotate.z=Math.PI/2;
+
   caballoMalla3.rotateX(Math.PI/2);
   caballoMalla3.translateY(3);
   caballoMalla3.translateX(60);
+  caballoMalla.rotate.z=-Math.PI/2;
   	//TORRES
   torreMalla = new Torre(TEXTURAS.ceramicablanca);
   torreMalla1 = new Torre(TEXTURAS.ceramicanegra);
