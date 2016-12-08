c2175ca01e681b9de9625c2f07f0e69aa755b71d
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-08 16:47:05 -0600

    Update Ajedrez_prueba_jorge.js

--- rv/Ajedrez_prueba_jorge.js
+++ rv/Ajedrez_prueba_jorge.js
@@ -281,13 +281,13 @@ function Caballo(textura){
 
   if(textura===TEXTURAS.ceramicablanca){
 	    this.side=1;
-	    this.pieizq.position.y=4;
-	    this.pieder.position.y=-4;
+	    this.pieizq.position.z=4;
+	    this.pieder.position.z=-4;
     }
     else if(textura===TEXTURAS.ceramicanegra){
 	    this.side=0;
-	    this.pieizq.position.y=-4;
-	    this.pieder.position.y=4;   
+	    this.pieizq.position.z=-4;
+	    this.pieder.position.z=4;   
     }
 }
 Caballo.prototype=new Agent();
