89abae993b8b14aac54b6a33e562f88feeb0dadc
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-07 19:36:46 -0600

    Update cavaio.js

--- rv/cavaio.js
+++ rv/cavaio.js
@@ -11,9 +11,9 @@ base3Forma.translate(0,2,0);
 troncoForma.translate(0,4,0);
 cubierta1Forma.translate(0,8,0);
 cubierta2Forma.translate(0,10,0);
-cabezaForma.translate(10,10,0);
-cabezaForma.rotateX(Math.PI/4);
-cabezaForma.rotateZ(Math.PI/4);
+cabezaForma.translate(0,10,0);
+//cabezaForma.rotateX(Math.PI/4);
+//cabezaForma.rotateZ(Math.PI/4);
 
 var base1Malla = new THREE.Mesh(base1Forma);
 var base2Malla= new THREE.Mesh(base2Forma);
