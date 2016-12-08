f399e5777967044a8592296e3af06b2a285af974
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-07 20:05:48 -0600

    Update cavaio.js

--- rv/cavaio.js
+++ rv/cavaio.js
@@ -3,7 +3,6 @@ var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
 var base3Forma = new THREE.CylinderGeometry(3,4,2,20,2,false);
 var troncoForma = new THREE.CylinderGeometry(3,3,6,20,6,false);
 var cubierta1Forma = new THREE.CylinderGeometry(4,3,2,20,2,false);
-var cubierta2Forma = new THREE.CylinderGeometry(4,4,2,20,3,false);
 
 var arco3 = new THREE.Shape();
 arco3.moveTo(0, 0);
@@ -17,7 +16,6 @@ base2Forma.translate(0,1,0);
 base3Forma.translate(0,2,0);
 troncoForma.translate(0,4,0);
 cubierta1Forma.translate(0,8,0);
-cubierta2Forma.translate(0,10,0);
 pico3.translate(0,12,0);
 
 var base1Malla = new THREE.Mesh(base1Forma);
@@ -25,7 +23,6 @@ var base2Malla= new THREE.Mesh(base2Forma);
 var base3Malla= new THREE.Mesh(base3Forma);
 var toncoMalla= new THREE.Mesh(troncoForma);
 var cubierta1Malla= new THREE.Mesh(cubierta1Forma);
-var cubierta2Malla= new THREE.Mesh(cubierta2Forma);
 var pico3Malla= new THREE.Mesh(pico3);
 
 var torreForma = new THREE.Geometry();
@@ -34,7 +31,6 @@ torreForma.merge(base2Malla.geometry, base2Malla.matrix);
 torreForma.merge(base3Malla.geometry, base3Malla.matrix);
 torreForma.merge(toncoMalla.geometry, toncoMalla.matrix);
 torreForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
-torreForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
 torreForma.merge(pico3Malla.geometry, pico3Malla.matrix);
 var material= new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh(torreForma, material);
