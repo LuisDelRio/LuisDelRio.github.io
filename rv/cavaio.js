d117aad639465e93ce9c7da5e89d4f32c5e1baa5
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-07 19:34:05 -0600

    Update cavaio.js

--- rv/cavaio.js
+++ rv/cavaio.js
@@ -4,18 +4,16 @@ var base3Forma = new THREE.CylinderGeometry(3,4,2,20,2,false);
 var troncoForma = new THREE.CylinderGeometry(3,3,6,20,6,false);
 var cubierta1Forma = new THREE.CylinderGeometry(4,3,2,20,2,false);
 var cubierta2Forma = new THREE.CylinderGeometry(4,4,2,20,3,false);
-var cabezaForma = new THREE.CylinderGeometry(1,5,15,10);
+var cabezaForma = new THREE.CylinderGeometry(1,5,10,10);
 
 base2Forma.translate(0,1,0);
 base3Forma.translate(0,2,0);
 troncoForma.translate(0,4,0);
 cubierta1Forma.translate(0,8,0);
 cubierta2Forma.translate(0,10,0);
-cabezaForma.translate(0,10,0);
+cabezaForma.translate(0,20,0);
 cabezaForma.rotateX(Math.PI/4);
 cabezaForma.rotateZ(Math.PI/4);
-
-
 
 var base1Malla = new THREE.Mesh(base1Forma);
 var base2Malla= new THREE.Mesh(base2Forma);
@@ -25,23 +23,22 @@ var cubierta1Malla= new THREE.Mesh(cubierta1Forma);
 var cubierta2Malla= new THREE.Mesh(cubierta2Forma);
 var cabezaMalla = new THREE.Mesh(cabezaForma);
 
-
-var torreForma = new THREE.Geometry();
-torreForma.merge(base1Malla.geometry, base1Malla.matrix);
-torreForma.merge(base2Malla.geometry, base2Malla.matrix);
-torreForma.merge(base3Malla.geometry, base3Malla.matrix);
-torreForma.merge(toncoMalla.geometry, toncoMalla.matrix);
-torreForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
-torreForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
-torreForma.merge(cabezaMalla.geometry, cabezaMalla.matrix);
+var caballoForma = new THREE.Geometry();
+caballoForma.merge(base1Malla.geometry, base1Malla.matrix);
+caballoForma.merge(base2Malla.geometry, base2Malla.matrix);
+caballoForma.merge(base3Malla.geometry, base3Malla.matrix);
+caballoForma.merge(toncoMalla.geometry, toncoMalla.matrix);
+caballoForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
+caballoForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
+caballoForma.merge(cabezaMalla.geometry, cabezaMalla.matrix);
 
 var material= new THREE.MeshNormalMaterial();
-var torreMalla = new THREE.Mesh(torreForma, material);
+var caballoMalla = new THREE.Mesh(caballoForma, material);
 
-torreMalla.rotateX(Math.PI/4);
+caballoMalla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
-escena.add(torreMalla);
+escena.add(caballoMalla);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=50;
 renderizador = new THREE.WebGLRenderer();
