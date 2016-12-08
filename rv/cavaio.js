d219ca0c97ba3f985d90c36903d09d52106bff6b
LuisDelRio <luis.antonio.delrio@hotmail.com>
2016-12-07 20:12:05 -0600

    Update cavaio.js

--- rv/cavaio.js
+++ rv/cavaio.js
@@ -3,7 +3,7 @@ var base2Forma = new THREE.CylinderGeometry(4,4,1,20,1,false);
 var base3Forma = new THREE.CylinderGeometry(3,4,2,20,2,false);
 var troncoForma = new THREE.CylinderGeometry(3,3,6,20,6,false);
 var cubierta1Forma = new THREE.CylinderGeometry(4,3,2,20,2,false);
-var cabezaForma = new THREE.CylinderGeometry(.25,5,2,20,2,false);
+var cabezaForma = new THREE.CylinderGeometry(.25,5,10,20,2,false);
 
 var arco3 = new THREE.Shape();
 arco3.moveTo(0, 0);
@@ -17,7 +17,7 @@ base2Forma.translate(0,1,0);
 base3Forma.translate(0,2,0);
 troncoForma.translate(0,4,0);
 cubierta1Forma.translate(0,8,0);
-cabezaForma.rotateZ(-Math.PI/4);
+cabezaForma.rotateZ(-Math.PI/2);
 cabezaForma.translate(0,12,0);
 pico3.translate(0,15,0);
 
